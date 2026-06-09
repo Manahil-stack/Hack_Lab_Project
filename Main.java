c48f021ee5044298bc0f3c8254946f08f33822fd
Manahil-stack <bscs14f25.15@gmail.com>
2026-06-09 20:33:32 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package hacklab;
+
+
+import hacklab.ui.SplashScreen;
+import javax.swing.SwingUtilities;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            SplashScreen splash = new SplashScreen();
+            splash.showAndProceed();
+        });
+    }
+}
